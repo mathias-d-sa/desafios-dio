35ac7b63da714f8db8f686b2d1b5c131baf1bc21
mathias-d-sa <handersonmathias14@gmail.com>
2023-03-23 13:09:28 -0300

    desafio Dragão - Daenerys Targaryen

---
+++ Dragao.java
@@ -0,0 +1,26 @@
+package br.com.desafios;
+
+import java.util.Scanner;
+
+public class Dragao {
+
+	public static void main(String[] args) {
+		int casos, poderDeLuta;
+
+		Scanner ler = new Scanner(System.in);
+
+		casos = ler.nextInt();
+
+		for (int i = 0; i < casos; i++) {
+			poderDeLuta = ler.nextInt();
+
+//TODO: Implemente a condição adequada para a impressão dos textos conforme o solicitado no desafio:
+
+			if (poderDeLuta < 8001) {
+				System.out.println("Inseto!");
+			} else {
+				System.out.println("Mais de 8000!");
+			}
+		}
+	}
+}
